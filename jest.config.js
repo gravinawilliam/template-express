6a18166cec913b60c9a0aaf5dab9fae5fa0bc7eb
William Gravina <dev.gravinawilliam@gmail.com>
2022-07-12 21:17:34 -0300

    🧹 chore: added library jest

---
+++ jest.config.js
@@ -0,0 +1,65 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('get-tsconfig').getTsconfig('./tsconfig.json')['config'];
+
+module.exports = {
+  verbose: true,
+  rootDir: './',
+  roots: ['<rootDir>'],
+  testEnvironment: 'node',
+  testEnvironmentOptions: {
+    NODE_ENV: 'test'
+  },
+  testMatch: ['<rootDir>/src/**/?(*.)+(unit|int|e2e|spec|test).(ts|js)'],
+  preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.json'
+    }
+  },
+  transform: {
+    '^.+\\.(t|j)sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            tsx: false,
+            decorators: true
+          },
+          target: 'es2021',
+          keepClassNames: true,
+          transform: {
+            legacyDecorator: true,
+            decoratorMetadata: true
+          }
+        },
+        module: {
+          type: 'es6',
+          noInterop: false
+        }
+      }
+    ]
+  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>' }),
+  modulePathIgnorePatterns: ['<rootDir>/dist'],
+  clearMocks: true,
+  collectCoverage: false,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/node_modules',
+    '<rootDir>/src/types',
+    '<rootDir>/src/index.ts',
+    '<rootDir>/src/healthcheck.ts',
+    '<rootDir>/src/main'
+  ],
+  reporters: ['default'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  }
+};
